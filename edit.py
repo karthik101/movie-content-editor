040ae9409bcfbc28df691e71e1e1c6f5be8188f7
AndrewNing <AndrewNing@ad7d335a-0a3e-11df-a6e1-f7bfe9b6b944>
2010-01-27 05:28:56 +0000

    Replaced edit.py.  Uses system clock and threading rather than run an infinite loop

--- edit.py
+++ edit.py
@@ -1,8 +1,22 @@
 import vlc
 import time
+import ctypes
+import sys
+import threading
+
+# TODO: not using any of these callback methods right now
+callbackmethod=ctypes.CFUNCTYPE(None, vlc.Event, ctypes.c_void_p) 
+
+@callbackmethod
+def func(event,data):
+    print "hi"
+    sys.exit(0)
+
+# TODO: remove hard-coded path  
+path = "C:\Users\YourName\Documents\Movie Editor\"
 
 # ------ pre-process sections to mute -----------
-f = open('C:\Users\Kimberly\Desktop\Movie Editer\mute.txt')
+f = open(path + "mute.txt")
 i = 0
 start = []
 finish = []
@@ -15,35 +29,60 @@ for line in f:
 # -------- Load and start movie ----------------
 instance = vlc.Instance()
 #instance.add_intf(None)
-media = instance.media_new("C:\Users\Kimberly\Movies (Full Size)\Kung Fu Panda\VIDEO_TS\VTS_10_1.vob")
+media = instance.media_new(path + "Kung Fu Panda.m4v")
 player = instance.media_player_new()
 player.set_media(media)
 player.play()
+mc = vlc.MediaControl(instance)
 # -------------------------------------------------
+
+
 
 # pause for one second to let player load
 time.sleep(1)
 
 # turn on subtitles
 #player.video_set_spu(1)
-player.video_set_subtitle_file('C:\Users\Kimberly\Desktop\Movie Editer\panda_edit.srt');
+player.video_set_subtitle_file(path + "panda_edit.srt');
 
 # (temporary) skipping over intro to get to good stuff
-player.set_time(20000)
+player.set_time(35000)
 
+startTime = time.time()*1000 - player.get_time()
 
-# ------- check for sections to mute (& skip - not implemented) ----------
-while player.is_playing():
-    
-    if player.get_time() > start[i] and player.get_time() < finish[i]:
-        instance.audio_set_mute(1)
-    else:
-        instance.audio_set_mute(0)
+event = vlc.EventType.MediaPlayerTimeChanged;
 
-    if player.get_time() > finish[i]:
-        i+=1
+eventManage = player.event_manager()
+#eventManage.event_attach(event,func,None)
 
-    # (temporary) to prevent movie from going on forever
-    if player.get_time() > 80000:
-        player.stop()
-# --------------------------------------------------------
+def mute(instance,tWait):
+    instance.audio_set_mute(1)
+    time.sleep(tWait)
+    instance.audio_set_mute(0)
+    print "muted"
+    return
+
+def skip(player,tSkip):
+    player.set_time(player.get_time() + long(tSkip*1000))
+    print "skipped"
+    return
+
+def stop(player):
+    print "done"
+    player.stop()
+    sys.exit(0)
+
+for i in range (0,len(start)):
+    tWait = (finish[i]-start[i])/1000
+    arg1 = (instance,tWait)
+    t1 = threading.Timer((start[i]-player.get_time())/1000,mute,arg1)
+    t1.start()
+
+#arg2 = (player,)
+#t2 = threading.Timer(80-player.get_time()/1000,stop,arg2)
+#t2.start()
+
+# this is temporary just so player doesn't go on for long time
+time.sleep(80-player.get_time()/1000)
+stop(player)
+

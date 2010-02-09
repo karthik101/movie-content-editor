ee6238a2205622024901017e5ebc7cc6e94888d5
AndrewNing <AndrewNing@ad7d335a-0a3e-11df-a6e1-f7bfe9b6b944>
2010-02-09 06:27:20 +0000

    added skip.  broke up mute to an onMute and an offMute.

--- edit.py
+++ edit.py
@@ -13,19 +13,21 @@ def sendDebug(msg, newline=1):
         print msg
 
 # For now I need to define the path, I commented out so it should work for you guys
-#path = 'C:\Users\Kimberly\Desktop\Andrew\Movie Editor\movie-editor'
-path = '.'
+#path = 'C:\Users\Kimberly\Desktop\Andrew\Movie Editor\movie-editor\\'
+path = ''
 
 # ------ pre-process sections to mute -----------
 try:
-    f = open(path + '\mute.txt','r')
+    f = open(path + 'mute.txt','r')
     i = 0
+    aType = []
     begin = []
     finish = []
     for line in f:
         separate = line.strip().split()
-        begin.append(float(separate[0])*1000)
-        finish.append(float(separate[1])*1000)
+        aType.append(int(separate[0]))
+        begin.append(float(separate[1])*1000)
+        finish.append(float(separate[2])*1000)
     f.close()
 except IOError:
     print("File not found")
@@ -34,8 +36,8 @@ except IOError:
 
 # -------- Load and start movie ----------------
 instance = vlc.Instance()
-#instance.add_intf(None)
-media = instance.media_new(path + "\Kung Fu Panda.m4v")
+instance.add_intf("qt")
+media = instance.media_new(path + "Kung Fu Panda.m4v")
 player = instance.media_player_new()
 player.set_media(media)
 player.play()
@@ -45,7 +47,7 @@ player.play()
 player.set_time(35000)
 
 # turn on subtitles
-player.video_set_subtitle_file(path + "\panda_edit.srt")
+player.video_set_subtitle_file(path + "panda_edit.srt")
 
 
 # ------------- subclass off of Thread ---------------
@@ -55,30 +57,36 @@ class editThread (Thread):
     # check in case it is interrupted by another thread.  
     def run ( self ):
         for i in range (0,len(begin)):
-            tAction = (finish[i] - begin[i])/1000
             
             # sleep until time for next action
             tSleep = (begin[i] - player.get_time())/1000
-            if (tSleep > 30): # don't know that 30 sec is the right number per se
+            if (tSleep > 30):
                 time.sleep(tSleep-30)
                 tSleep = (begin[i] - player.get_time())/1000
             time.sleep(tSleep)
             
-            mute(tAction)
+            # perform action
+            if (aType[i] == 0):
+                onMute()
+            elif (aType[i] == 1):
+                offMute()
+            elif (aType[i] == 2):
+                skip((finish[i] - begin[i])/1000)
+                
         return
 # ------------------------------------------------------
 
 # ------- methods -------------------------
-def mute (tMute):
+def onMute ():
     instance.audio_set_mute(1)
-    time.sleep(tMute)
-    instance.audio_set_mute(0)
     return
     
+def offMute ():
+    instance.audio_set_mute(0)
+    return
 
 def skip(tSkip):
     player.set_time(player.get_time() + long(tSkip*1000))
-    sendDebug("skipped")
     return
 
 def stop(player):
